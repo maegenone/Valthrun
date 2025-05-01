7b9854803e8f5488817dc85cf3a4482db7d9777c
maegen <rtzfeng@gmail.com>
2025-05-01 12:06:43 +0800

    Update grenade_helper.rs
    Fix Grenade info UI element with no interaction (mouse click thru the background ui) and just show text floating.

--- controller/src/enhancements/grenade_helper.rs
+++ controller/src/enhancements/grenade_helper.rs
@@ -331,9 +331,12 @@ impl Enhancement for GrenadeHelper {
                             Condition::Always,
                         )
                         .position_pivot([0.0, 0.5])
-                        .no_decoration()
+                        .flags(
+                            imgui::WindowFlags::NO_DECORATION
+                                | imgui::WindowFlags::NO_INPUTS
+                                | imgui::WindowFlags::ALWAYS_AUTO_RESIZE
+                        )
                         .draw_background(settings.grenade_background)
-                        .always_auto_resize(true)
                         .begin()
                     {
                         ui.text_colored(color.as_f32(), &grenade.name);
